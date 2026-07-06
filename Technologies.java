4865e436f358cc80be4887205d34d25fcdf2f17e
karthikmanchala23-cell <karthikmanchala23@gmail.com>
2026-07-06 12:15:33 +0530

    Delete Technologies.java

--- Technologies.java
+++
@@ -1,8 +0,0 @@
-public class Technologies{
-    public static void main(String[] args){
-        if(args.length != 2){
-            System.out.println("Please provide two numbers as command line argument");
-        }
-        System.out.println(args[0] + "Technologies" + args[1]);
-    }
-}
