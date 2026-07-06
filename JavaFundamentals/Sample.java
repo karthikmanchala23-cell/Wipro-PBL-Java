7570ce8d265bbc87406861ce7b894b1a580bf77a
karthikmanchala23-cell <karthikmanchala23@gmail.com>
2026-07-06 11:52:28 +0530

    Add files via upload

---
+++ JavaFundamentals/Sample.java
@@ -0,0 +1,9 @@
+public class Sample{
+    public static void main(String[] args){
+        if(args.length != 1){
+            System.out.println("Please provide one number as command line argument");
+            return;
+        }
+        System.out.println("Welcome "+ args[0]);
+    }
+}
