397ec4c612d3b6ca4452e06658ef700baf21b407
karthikmanchala23-cell <karthikmanchala23@gmail.com>
2026-07-06 12:14:27 +0530

    Delete Sample.java

--- Sample.java
+++
@@ -1,9 +0,0 @@
-public class Sample{
-    public static void main(String[] args){
-        if(args.length != 1){
-            System.out.println("Please provide one number as command line argument");
-            return;
-        }
-        System.out.println("Welcome "+ args[0]);
-    }
-}
