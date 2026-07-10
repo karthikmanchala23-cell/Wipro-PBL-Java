8853de9964ab473e23caa5a87700aea36e266bfc
karthikmanchala23-cell <karthikmanchala23@gmail.com>
2026-07-10 09:32:33 +0530

    Add files via upload

---
+++ OOPS/Optional/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+
+    int id;
+    String name;
+
+    Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("Employee Id : " + id);
+        System.out.println("Employee Name : " + name);
+    }
+}
