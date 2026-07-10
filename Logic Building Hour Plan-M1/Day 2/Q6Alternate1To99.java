34f8664317ac11b29a2ac269700656f0e927c596
karthikmanchala23-cell <karthikmanchala23@gmail.com>
2026-07-10 09:42:16 +0530

    Add files via upload

---
+++ Logic Building Hour Plan-M1/Day 2/Q6Alternate1To99.java
@@ -0,0 +1,10 @@
+public class Q6Alternate1To99 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 99; i += 2) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
---
+++ Logic Building Hour Plan-M1/Day 2/Q7Alternate0To100.java
@@ -0,0 +1,10 @@
+public class Q7Alternate0To100 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
