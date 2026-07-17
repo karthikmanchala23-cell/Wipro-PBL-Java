19872bfe7f05d9fb015b0736a9f1a4384634c845
karthikmanchala23-cell <karthikmanchala23@gmail.com>
2026-07-17 10:52:43 +0530

    Add files via upload

---
+++ Abstraction/Interfaces/music/string/Q1Veena.java
@@ -0,0 +1,10 @@
+package music.string;
+
+import music.Q1Playable;
+
+public class Q1Veena implements Q1Playable {
+
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
