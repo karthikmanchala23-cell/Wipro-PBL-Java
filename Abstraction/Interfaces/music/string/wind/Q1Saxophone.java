745ff426669227438c4df7fcbd6c881ca92e4a78
karthikmanchala23-cell <karthikmanchala23@gmail.com>
2026-07-17 10:54:04 +0530

    Add files via upload

---
+++ Abstraction/Interfaces/music/string/wind/Q1Saxophone.java
@@ -0,0 +1,10 @@
+package music.wind;
+
+import music.Q1Playable;
+
+public class Q1Saxophone implements Q1Playable {
+
+    public void play() {
+        System.out.println("Playing Saxophone");
+    }
+}
